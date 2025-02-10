a23ce74d189624fa8600f4019769dbc9b398744d
alessioc17 <160236106+alessioc17@users.noreply.github.com>
2025-02-10 14:46:50 +0100

    Update 06_pet_preproc_SpanormFDG.py

--- 06_pet_preproc_SpanormFDG.py
+++ 06_pet_preproc_SpanormFDG.py
@@ -7,11 +7,9 @@ Created on Fri Aug 11 16:17:57 2023
 @email: alessio.cirone@hsanmartino.it; campi@dima.unige.it; garbarino@dima.unige.it
 """
 
-import sys
-import os
+import sys, os, time
 from nipype.interfaces.freesurfer import MRIConvert
 from nipype.interfaces.ants import RegistrationSynQuick, ApplyTransforms
-import time
 from multiprocessing import cpu_count
 from joblib import Parallel, delayed
 os.environ['FREESURFER_HOME'] = '/usr/local/freesurfer/7.4.1/'
@@ -34,65 +32,62 @@ def ciclo_spanormFDG(subj_id, path_data):
             reg_MR.inputs.output_prefix = 'MRonMNI_'
             reg_MR.run()
             
-            ### GTMSeg -> MNI
-            gtmseg = path_data + subj_id + '/mri/gtmseg.mgz'
-            if os.path.exists(gtmseg):
-                segconv = MRIConvert()
-                segconv.inputs.in_file = gtmseg
-                segconv.inputs.out_file = path_data + subj_id + '/mri/gtmseg.nii.gz'
-                segconv.inputs.out_type = 'niigz'
-                segconv.run()
-                at_seg = ApplyTransforms()
-                at_seg.inputs.input_image = path_data + subj_id + '/mri/gtmseg.nii.gz'
-                at_seg.inputs.reference_image = MNI_T1
-                at_seg.inputs.output_image = path_data + subj_id + '/mri/gtmseg_on_mni.nii.gz'
-                at_seg.inputs.transforms = [mri_registered + 'MRonMNI_1Warp.nii.gz', mri_registered + 'MRonMNI_0GenericAffine.mat']
-                at_seg.inputs.interpolation = 'NearestNeighbor'
-                at_seg.run()
+        ### Seg -> MNI
+        seg = path_data + subj_id + '/mri/aparc+aseg.mgz'
+        if os.path.exists(seg):
+            segconv = MRIConvert()
+            segconv.inputs.in_file = seg
+            segconv.inputs.out_file = path_data + subj_id + '/mri/aparc+aseg.nii.gz'
+            segconv.inputs.out_type = 'niigz'
+            segconv.run()
+            at_seg = ApplyTransforms()
+            at_seg.inputs.input_image = path_data + subj_id + '/mri/aparc+aseg.nii.gz'
+            at_seg.inputs.reference_image = MNI_T1
+            at_seg.inputs.output_image = path_data + subj_id + '/mri/aparc+aseg_on_mni.nii.gz'
+            at_seg.inputs.transforms = [mri_registered + 'MRonMNI_1Warp.nii.gz', mri_registered + 'MRonMNI_0GenericAffine.mat']
+            at_seg.inputs.interpolation = 'NearestNeighbor'
+            at_seg.run()
                     
-                ### FDG -> MNI
-                fdgpet = path_data + subj_id + '/fdgpet/orig/fdg.nii'
-                if os.path.exists(fdgpet):
-                    fdgpet_registered = path_data + subj_id + '/fdgpet/norm/'
-                    if not os.path.exists(fdgpet_registered):
-                        os.makedirs(fdgpet_registered)
-                    os.chdir(fdgpet_registered)
-                    reg_fdg = RegistrationSynQuick()
-                    reg_fdg.inputs.fixed_image = MRI_T1
-                    reg_fdg.inputs.moving_image = fdgpet
-                    reg_fdg.inputs.transform_type = "a"
-                    reg_fdg.inputs.output_prefix = 'FDGPETonMR_'
-                    reg_fdg.run()
-                    at_fdg = ApplyTransforms()
-                    at_fdg.inputs.input_image = 'FDGPETonMR_Warped.nii.gz'
-                    at_fdg.inputs.reference_image = MNI_T1
-                    at_fdg.inputs.output_image = 'FDGPETonMNI.nii.gz'
-                    at_fdg.inputs.transforms = [mri_registered + 'MRonMNI_1Warp.nii.gz', mri_registered + 'MRonMNI_0GenericAffine.mat']
-                    at_fdg.run()
-                    print("\n" + "########### " + "Subject " + subj_id + " complete " + "###########" + "\n")
-                    f1 = open(path_project + 'logSubjects_spanormFDG.txt', 'a')
-                    f1.write("SpanormFDG complete for subject " + subj_id + "\n")
-                    f1.close()
-                else:
-                    print("Unable to run SpanormFDG. FDG-PET is missing in Subject", subj_id)
-                    f2 = open(path_project + 'logSubjects_spanormFDG_warning.txt', 'a')
-                    f2.write("Unable to run SpanormFDG. FDG-PET is missing in Subject " + subj_id + "\n")
-                    f2.close()
+            ### PET -> MNI
+            pet = path_data + subj_id + '/amypet/orig/amy.nii'
+            if os.path.exists(pet):
+                pet_registered = path_data + subj_id + '/amypet/norm/'
+                if not os.path.exists(pet_registered):
+                    os.makedirs(pet_registered)
+                os.chdir(pet_registered)
+                reg_fdg = RegistrationSynQuick()
+                reg_fdg.inputs.fixed_image = MRI_T1
+                reg_fdg.inputs.moving_image = pet
+                reg_fdg.inputs.transform_type = "a"
+                reg_fdg.inputs.output_prefix = 'AMYPETonMR_'
+                reg_fdg.run()
+                at_fdg = ApplyTransforms()
+                at_fdg.inputs.input_image = 'AMYPETonMR_Warped.nii.gz'
+                at_fdg.inputs.reference_image = MNI_T1
+                at_fdg.inputs.output_image = 'AMYPETonMNI.nii.gz'
+                at_fdg.inputs.transforms = [mri_registered + 'MRonMNI_1Warp.nii.gz', mri_registered + 'MRonMNI_0GenericAffine.mat']
+                at_fdg.run()
+                
+                print("\n" + "########### " + "Subject " + subj_id + " complete " + "###########" + "\n")
+                f1 = open(path_project + 'logSubjects_spanorm.txt', 'a')
+                f1.write("Spanorm complete for subject " + subj_id + "\n")
+                f1.close()
             else:
-                print("Unable to run SpanormFDG. gtmseg is missing in Subject", subj_id)
-                f2 = open(path_project + 'logSubjects_spanormFDG_warning.txt', 'a')
-                f2.write("Unable to run SpanormFDG. GTMSeg is missing in Subject " + subj_id + "\n")
+                print("Unable to run Spanorm. PET is missing in Subject", subj_id)
+                f2 = open(path_project + 'logSubjects_spanorm_warning.txt', 'a')
+                f2.write("Unable to run Spanorm. PET is missing in Subject " + subj_id + "\n")
                 f2.close()
         else:
-            print("Unable to run SpanormFDG. T1 is missing in Subject", subj_id)
+            print("Unable to run SpanormFDG. gtmseg is missing in Subject", subj_id)
             f2 = open(path_project + 'logSubjects_spanormFDG_warning.txt', 'a')
-            f2.write("Unable to run SpanormFDG. T1 is missing in Subject " + subj_id + "\n")
+            f2.write("Unable to run SpanormFDG. GTMSeg is missing in Subject " + subj_id + "\n")
             f2.close()
 
 if __name__ == '__main__':
     sys.setrecursionlimit(5000)
     path_project = '/home/shared/LISCOMP/PD_HSM/'
     path_data = path_project + 'data/'
+    MNI_T1 = '/home/shared/LISCOMP/Healthy_MNI/mni_icbm152_t1_tal_nlin_sym_55_ext.nii.gz'
     os.environ['SUBJECTS_DIR'] = path_data
     subj_List = sorted(os.listdir(path_data))
     start_time = time.time()
