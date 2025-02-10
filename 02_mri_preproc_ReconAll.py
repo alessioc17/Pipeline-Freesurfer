6292038ba99cb27fccdda343945c86a28ab06609
alessioc17 <160236106+alessioc17@users.noreply.github.com>
2025-02-10 11:41:30 +0100

    Updating reconall script

--- 02_mri_preproc_ReconAll.py
+++ 02_mri_preproc_ReconAll.py
@@ -14,7 +14,10 @@ from nipype.interfaces.freesurfer import ReconAll
 import time
 from multiprocessing import cpu_count
 from joblib import Parallel, delayed
+
 os.environ['FREESURFER_HOME'] = '/usr/local/freesurfer/7.4.1/'
+os.environ["OMP_NUM_THREADS"] = "1"  # Limita i thread OpenMP globalmente a 1
+os.environ["ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS"] = "1"  # per FreeSurfer
 
 def ciclo_reconAll(subj_id, path_data):
     try:
@@ -23,6 +26,7 @@ def ciclo_reconAll(subj_id, path_data):
         os.remove(path_data + subj_id + '/scripts/IsRunning.rh')
     except:
         pass
+    
     T1_mgz = path_data + subj_id + '/mri/orig/001.mgz'
     if os.path.exists(T1_mgz):
         reconall = ReconAll()
@@ -32,22 +36,55 @@ def ciclo_reconAll(subj_id, path_data):
         reconall.inputs.T1_files = T1_mgz
         reconall.inputs.parallel = True
         reconall.run()
-        print("\n" + "########### " + "Subject " + subj_id + " complete " + "###########" + "\n")
-        f1 = open(path_project + 'logSubjects_reconAll.txt', 'a')
-        f1.write("ReconAll complete for subject " + subj_id + "\n")
-        f1.close()
+        
+        print(f"\n########### Subject {subj_id} complete ###########\n")
+        with open(path_project + 'logSubjects_reconAll.txt', 'a') as f1:
+            f1.write(f"ReconAll complete for subject {subj_id}\n")
     else:
-        print("Unable to run ReconAll. T1 is missing in: " + subj_id)
-        f2 = open(path_project + 'logSubjects_reconAll_warning.txt', 'a')
-        f2.write("Unable to run ReconAll. T1 is missing in: " + subj_id + "\n")
-        f2.close()
-    
+        print(f"Unable to run ReconAll. T1 is missing in: {subj_id}")
+        with open(path_project + 'logSubjects_reconAll_warning.txt', 'a') as f2:
+            f2.write(f"Unable to run ReconAll. T1 is missing in: {subj_id}\n")
+
+def check_failed_subjects(subj_List, path_data):
+    failed_subjects = []
+    for subj_id in subj_List:
+        status_file = os.path.join(path_data, subj_id, 'scripts', 'recon-all-status.log')
+        if os.path.exists(status_file):
+            with open(status_file, 'r') as f:
+                last_line = f.readlines()[-1].strip()
+                if not last_line.startswith(f"recon-all -s {subj_id} finished without error"):
+                    failed_subjects.append(subj_id)
+        else:
+            failed_subjects.append(subj_id)
+    return failed_subjects
+
+def process_in_batches(subj_List, path_data, batch_size=24):
+    total_subjects = len(subj_List)
+    for i in range(0, total_subjects, batch_size):
+        batch = subj_List[i:i+batch_size]
+        Parallel(n_jobs=int(cpu_count()))(delayed(ciclo_reconAll)(subj_id, path_data) for subj_id in batch)
+        print(f"Batch {i//batch_size + 1} completed")
+
 if __name__ == '__main__':
     sys.setrecursionlimit(5000)
-    path_project = '/home/shared/LISCOMP/PD_HSM/'
+    path_project = '/workspaces/testpippo/Data/HBD/harmonized_with_stargan_to_Gyroscan/star_Guys_harm_temp/'
     path_data = path_project + 'data/'
     os.environ['SUBJECTS_DIR'] = path_data
     subj_List = sorted(os.listdir(path_data))
+    
     start_time = time.time()
-    ris = Parallel(n_jobs=int(cpu_count()))(delayed(ciclo_reconAll)(subj_id, path_data) for subj_id in subj_List)
-    print("--- %s seconds (for reconaAll) ---" % (time.time() - start_time))
+    process_in_batches(subj_List, path_data)
+    
+    first_failed = check_failed_subjects(subj_List, path_data)
+    if first_failed:
+        with open(path_project + 'logSubjects_failed_once.txt', 'w') as f:
+            f.write("\n".join(first_failed) + "\n")
+        print(f"Retrying {len(first_failed)} subjects that failed the first time...")
+        process_in_batches(first_failed, path_data)
+        second_failed = check_failed_subjects(first_failed, path_data)
+        if second_failed:
+            with open(path_project + 'logSubjects_failed_twice.txt', 'w') as f:
+                f.write("\n".join(second_failed) + "\n")
+            print(f"Warning: {len(second_failed)} subjects failed twice. Check logSubjects_failed_twice.txt")
+    
+    print(f"--- {time.time() - start_time} seconds (for recon-all) ---")
